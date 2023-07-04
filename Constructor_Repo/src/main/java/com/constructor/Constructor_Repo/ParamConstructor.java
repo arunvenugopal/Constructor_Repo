38161e211fe4d627d65c4e2b07d66e9be4a77dce
HP <HP@192.168.1.102>
2023-07-04 23:45:57 +0530

    Commit for Constructor program

---
+++ Constructor_Repo/src/main/java/com/constructor/Constructor_Repo/ParamConstructor.java
@@ -0,0 +1,20 @@
+package com.constructor.Constructor_Repo;
+
+public class ParamConstructor {
+
+	public ParamConstructor(int id, String name)    // Parametrized Constructor creation.   only 1 constructor creation
+	{
+		System.out.println();
+		System.out.println("The ID is: "+id);
+		System.out.println();
+		System.out.println("The name is: "+name);
+	}
+
+
+	public static void main(String []args)
+	{
+		ParamConstructor s=new ParamConstructor(10,"Arun V");            // will give 10 students details
+        ParamConstructor m=new ParamConstructor(11,"Mahesh C"); 		// calling the parametrized constructor. No need of any separate calling. While calling just passing the values
+	}
+
+}
